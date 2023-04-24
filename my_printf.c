8aaaf3150e1fcb6852e1da41d05e789803a3e26b
lloydwein <oge.otukpa@gmail.com>
2023-04-24 03:45:58 -0700

    0x11. C - Printf

--- my_printf.c
+++ my_printf.c
@@ -1,99 +1,49 @@
 #include "main.h"
 
-void apply_operation(const char *format, int *num, int *cnt, op_t ops[],
-		va_list arg_ptr, int *found);
-
 /**
- * apply_operation - processes the format strings
- * Description: a function that processes the format string based on th
- * operations defined in the 'ops' array and the arguments provided in the
- * 'arg_ptr' list.
- * @format: pointer to a character array representing the format string
- * @num: pointer to an int representing current position of the iterator
- * @cnt: pointer to an int representing the character count processed so fa
- * @ops: array of structures that define the operations to be applies to
- * format string
- * @arg_ptr: pointer to list of arguments to be processes by format string
- * @found: point to integer representing whether specifier is found in format
- * string or not
- * Return: no return value
+ * _printf - prints output according to a format
+ * @format: format string
+ * Return: number of characters printed
  */
-
-void apply_operation(const char *format, int *num, int *cnt, op_t ops[],
-		va_list arg_ptr, int *found)
-{
-	int index, chars;
-
-	if (*(format + *num + 1) == '%')
-	{
-		_putchar('%');
-		*cnt += 1;
-		*found = 1;
-		*num += 1;
-	}
-	else
-	{
-		index = 0;
-		while ((ops + index)->op)
-		{
-			if (*((ops + index)->op) == *(format + *num + 1))
-			{
-				chars = (ops + index)->f(arg_ptr);
-				*cnt += chars;
-				*found = 1;
-				*num += 1;
-				break;
-			}
-			index++;
-		}
-	}
-}
-
-int _printf(const char *format, ...);
-
-/**
- * _printf - print formatted output to the standard output stream
- * @format: string of characters
- * Return: integer, which represents total numbeer of characters to be printed
- */
-
 int _printf(const char *format, ...)
 {
-	va_list arg_ptr;
-	int num, cnt = 0, found = 0;
-	op_t ops[] = {
-		{"c", character}, {"s", string}, {"d", integer}, {"i", integer},
-		{"u", u_integer}, {"b", binary}, {"o", octal}, {"x", hexa_lower},
-		{"X", hexa_upper}, {"S", custom_string}, {NULL, NULL}
-	};
+	char *buff;
+	int index = 0, count = 0;
+	va_list args;
+	int (*func)(char *, int, va_list);
 
-	va_start(arg_ptr, format);
+	buff = malloc(4000);
+	if (!buff)
+		exit(1);
 
-	if (!format || !format[1])
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 	{
+		free(buff);
 		return (-1);
 	}
-	while (*(format + num))
-	{
-		found = 0;
-		if (*(format + num) == '%')
-		{
-			apply_operation(format, &num, &cnt, ops, arg_ptr, &found);
+	va_start(args, value);
 
-			if (found == 0 && *(format + num + 1) != '\0')
-			{
-				_putchar(*(format + num));
-				cnt++;
-			}
+	while (format[index])
+	{
+		if (format[index] != '%')
+		{
+			buff[count] = format[index];
+			count++;
 		}
 		else
 		{
-			_putchar(*(format + num));
-			cnt++;
+			func = check_param(format[index + 1]);
+			if (!func)
+				buff[count] = '%';
+				count++;
+			else
+				count = func(&buff[count], count, args);
+			index++;
 		}
-		num++;
+		index++;
 	}
-	va_end(arg_ptr);
-
-	return (cnt);
+	write(1, buff, count);
+	va_end(args);
+	free(buff);
+	return (count);
 }
